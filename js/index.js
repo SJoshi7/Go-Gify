6481f100b8995a7dbed9c3cdb4c6ae4385b9b715
Sjoshi7 <sjoshise7@gmail.com>
2019-06-16 05:07:53 +0530

    pagination basics

--- js/index.js
+++ js/index.js
@@ -1,21 +1,30 @@
-searchGif = () => {
+searchGif = (id) => {
+
+document.getElementById(id).className = "active black";
+
+for(i=1;i<6;i++){
+	if(i!=id){
+		document.getElementById(i).className = "waves-effect";
+	}
+}
 
 const app = document.getElementById('content-div');
-
-var api = "https://api.giphy.com/v1/gifs/search?";
-var apiKey = "&api_key=6gB95G4VQIE6zjRlryyFgXjTpxfRzCnf&q=";
+const api = "https://api.giphy.com/v1/gifs/search?";
+const apiKey = "&api_key=6gB95G4VQIE6zjRlryyFgXjTpxfRzCnf&q=";
 var searchTerm = document.getElementById("query").value.toLowerCase();
-var searchUrl = api + apiKey + searchTerm;
+const limit = "&limit=30";
+var searchUrl = api + apiKey + searchTerm + limit;
 
 var request = new XMLHttpRequest();
 request.open('GET',searchUrl,true);
 
 request.onload = function(){
 	var dataFetched = JSON.parse(this.response);
+	console.log(dataFetched);
 	
 	if(request.status>=200 && request.status<=400){
 		document.getElementById("content-div").innerHTML = "";
-		for(var i=0;i<6;i++){
+		for(var i=(id-1)*6;i<6*id;i++){
 			const gifDiv = document.createElement('div');
 			gifDiv.setAttribute('class','col s12 m4 l3 gif-div');
 			app.appendChild(gifDiv);
@@ -23,6 +32,7 @@ request.onload = function(){
 			newgif.setAttribute('src', dataFetched.data[i].images.original.url);
 			gifDiv.appendChild(newgif);
 		}
+
 	}
 	else{
 		console.log("error"); //to be edited

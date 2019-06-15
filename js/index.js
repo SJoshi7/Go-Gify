cca9bc93cc063841ddf5c5d054c55c2393933ab0
Sjoshi7 <sjoshise7@gmail.com>
2019-06-16 01:07:50 +0530

    basic functionality done

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
-runTimer = () => {
+searchGif = () => {
 
 const app = document.getElementById('content-div');
 
-//the url
-var apikey = "6gB95G4VQIE6zjRlryyFgXjTpxfRzCnf";
-var lmt = 6;
+var api = "https://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=6gB95G4VQIE6zjRlryyFgXjTpxfRzCnf&q=";
 var searchTerm = document.getElementById("query").value.toLowerCase();
-var searchUrl = "https://api.giphy.com/v1/gifs/random?api_key=" + apikey + "&q=" +
-            searchTerm + "&limit=" + lmt+"&offset=0rating=G&lang=en";
+var searchUrl = api + apiKey + searchTerm;
 
-//starting request
 var request = new XMLHttpRequest();
 request.open('GET',searchUrl,true);
 
 request.onload = function(){
 	var dataFetched = JSON.parse(this.response);
-	console.log(dataFetched["data"].url);
+	console.log(dataFetched.data[0].images.original.url);
+	console.log(dataFetched.data.length);
+	
 	if(request.status>=200 && request.status<=400){
-		const gifDiv = document.createElement('div');
-		gifDiv.setAttribute('class','col s12 m4 l3 gif-div');
-		app.appendChild(gifDiv);
-		const newgif = document.createElement('img');
-		newgif.setAttribute('src', dataFetched["data"].url);
-		gifDiv.appendChild(newgif);
+		document.getElementById("content-div").innerHTML = "";
+		for(var i=0;i<6;i++){
+			const gifDiv = document.createElement('div');
+			gifDiv.setAttribute('class','col s12 m4 l3 gif-div');
+			app.appendChild(gifDiv);
+			const newgif = document.createElement('img');
+			newgif.setAttribute('src', dataFetched.data[i].images.original.url);
+			gifDiv.appendChild(newgif);
+		}
 	}
 	else{
 		console.log("error"); //to be edited
